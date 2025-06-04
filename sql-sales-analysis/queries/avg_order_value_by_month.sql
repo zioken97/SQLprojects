f321e90459b8c5f162c9b4d9dd8e683fc86946b1
zioken97 <fra.leo.info@protonmail.com>
2025-06-04 11:48:58 -0400

    Delete sql-sales-analysis directory

--- sql-sales-analysis/queries/avg_order_value_by_month.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  EXTRACT(YEAR FROM sale_date) AS year,
-  EXTRACT(MONTH FROM sale_date) AS month,
-  ROUND(AVG(quantity * unit_price), 2) AS avg_order_value
-FROM sales
-GROUP BY year, month
-ORDER BY year, month;
