0f8f75dee9158b0b178a33496022973ca762c031
francescoleonardi <fra.leo.info@protonmail.com>
2025-06-05 21:05:04 -0400

    Create schema.sql

---
+++ Customer Data Cleaning/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE raw_customers (
+    customer_id INT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    signup_date VARCHAR(20),
+    country VARCHAR(50)
+);
